39c3444a4ddbd3db7d3f23438c018c0d5c3c5bc3
Amanda Lima <amandalimasiva@gmail.com>
2022-12-27 00:17:15 -0300

    Refaturando código

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,29 +1,32 @@
 ///reference types="cypress" />
 import { faker } from "@faker-js/faker";
 
-it("Realiza um cadastro", () => {
-  cy.visit("/");
-  cy.title().should("eq", "QAZANDO Shop E-Commerce");
+describe("Cadastro de usuário", () => {
+  before(() => {
+    cy.visit("/");
+    cy.title().should("eq", "QAZANDO Shop E-Commerce");
+  });
+  it("Informa um novo e-mail", () => {
+    cy.contains("a", "Cadastro").click("center");
 
-  cy.contains("a", "Cadastro").click("center");
+    cy.contains("h3", "Cadastro de usuário").should("be.visible");
 
-  cy.contains("h3", "Cadastro de usuário").should("be.visible");
+    const user = {
+      name: "Amanda LS",
+      email: faker.internet.email(),
+      password: "pwqwe583",
+    };
 
-  const user = {
-    name: "Amanda LS",
-    email: faker.internet.email(),
-    password: "pwqwe583",
-  };
+    cy.get("#user").type(user.name);
+    cy.get("#email").type(user.email);
+    cy.get("#password").type(user.password);
 
-  cy.get("#user").type(user.name);
-  cy.get("#email").type(user.email);
-  cy.get("#password").type(user.password);
+    cy.get("#btnRegister").click();
 
-  cy.get("#btnRegister").click();
+    cy.get(".swal2-popup").should("be.visible");
 
-  cy.get(".swal2-popup").should("be.visible");
+    cy.get(".swal2-popup").find("#swal2-title");
 
-  cy.get(".swal2-popup").find("#swal2-title");
-
-  cy.get(".swal2-confirm").click();
+    cy.get(".swal2-confirm").click();
+  });
 });
